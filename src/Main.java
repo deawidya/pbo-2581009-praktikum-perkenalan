1c2d04f0e887b1a3fc4d7aafe6a2ac0149898eea
Dea Widya Ningsih <2581009@unai.edu>
2026-09-11 09:25:50 +0700

    menambahkan nama anda dan nim

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("Masukkan NIM: ");
+        int nim= Integer.parseInt(scanner.nextLine());
+
+    }
+}
