8ef72818e4620a65a8ef9172346584df7a35566a
Dea Widya Ningsih <2581009@unai.edu>
2026-09-11 09:53:05 +0700

    mengubah typo pada coding

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,16 @@ public class Main {
         String nama = scanner.nextLine();
 
         System.out.print("Masukkan NIM: ");
-        int nim= Integer.parseInt(scanner.nextLine());
+        int nim= Integer.parseInt
+                (scanner.nextLine());
 
         System.out.print("Masukkan umur: ");
-        int umur = Integer.parseInt(scanner.nextLine());
+        int umur = Integer.parseInt
+                (scanner.nextLine());
 
         System.out.print("Masukkan tinggi: ");
-        double tinggi = Double.parseDouble(scanner.nextLine());
+        double tinggi = Double.parseDouble
+                (scanner.nextLine());
 
         System.out.println("Hallo! Nama saya " + nama + ", NIM: " + nim + ", Umur: " + umur + " Tahun, Tinggi: " + tinggi + " cm");
     }
